e2ad22344d7ba7df6c1442339affa8a94da0db53
Svetosa <Osadchenkosvetlana2006@gmail.com>
2024-09-09 19:39:05 +0300

    Вывод в консоль Hello World и ввод числа

--- lab0/lab0/lab0.cpp
+++ lab0/lab0/lab0.cpp
@@ -1,5 +1,11 @@
 ﻿#include <iostream>
-
+using namespace std;
 int main()
 {
+	cout << "Hello World!" << endl;
+	int s;
+	cout << "Enter random number: ";
+	cin >> s;
+	cout << "Your number is " << s;
+
 }
