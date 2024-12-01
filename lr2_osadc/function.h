addcb27dba00b49d49259e8b48d7e56057573064
Svetosa <Osadchenkosvetlana2006@gmail.com>
2024-12-01 21:10:30 +0300

    Создала main.cpp, pipe.h, function.h, cs.h, function.cpp
    main.cpp - Создана функция menu(). Она реализует вывод меню в консоль. Основная функция int main(). В ней происходит создание бесконечного цикла, команды вызываются с помощью switch - case. Реализована базовая проверка: если пользователь вводит цифры не из меню, программа выдает повторный ввод
    function.cpp - реализованы основные функции для работы программы : логирование, добавления трубы и кс, сохранение, загрузка и т.д.
    function.h - определение функций и проверка введенных данных
    cs.h и pipe.h - описание классов Pipe и CS, инициализировала переменные

---
+++ lr2_osadc/function.h
@@ -0,0 +1,162 @@
+
+
+
+void pipeadd();
+void Print();
+void loggir(const string& message);
+void addCS();
+void save();
+void load();
+void filter();
+
+template<typename T>
+T getInput(string str, int min, int max) {
+	string input;
+	T result;
+	if constexpr (is_same<T, string>::value) {
+		while (true) {
+			cout << str;
+			getline(cin, input);
+			loggir(input);
+			if (input.empty()) {
+				cout << "Вы ничего не ввели, попробуйте снова :)" << endl << endl;
+				continue;
+			}
+			result = input;
+			return result;
+		}
+	}
+	else if constexpr (is_same<T, int>::value) {
+		while (true) {
+			cout << str;
+			getline(cin, input);
+			loggir(input);
+			if (input.empty()) {
+				cout << "Вы ничего не ввели, попробуйте снова :)" << endl << endl;
+				continue;
+			}
+
+			bool isValid = true;
+			for (char c : input) {
+				if (!isdigit(c)) {
+					cout << "Введено неправильное число, повторите снова :)" << endl << endl;
+					isValid = false;
+					break;
+				}
+			}
+			if (isValid) {
+				try {
+					if (input[0] == '0' && input.size() > 1) {
+						cout << "Число не может начинаться с 0, попробуйте снова :)" << endl << endl;
+						continue;
+					}
+					result = stoi(input);
+					if (min <= result && result <= max) {
+						return result;
+					}
+					else {
+						cout << "введено неверное значение, попробуйте снова :)" << endl << endl;
+					}
+				}
+				catch (const invalid_argument& e) {
+					cout << "Ошибка ввода числа " << endl << endl;
+				}
+				catch (const out_of_range& e) {
+					cout << "Число вне диапазона значений " << endl << endl;
+				}
+			}
+		}
+	}
+	else if constexpr (is_same<T, float>::value || is_same<T, double>::value) {
+		while (true) {
+			cout << str;
+			getline(cin, input);
+			loggir(input);
+			if (input.empty()) {
+				cout << "Вы ничего не ввели, попробйте снова" << endl << endl;
+				continue;
+			}
+
+			try {
+				size_t pos;
+				result = stof(input, &pos);
+				if (pos < input.size()) {
+					cout << "Ошибка: некорректный ввод числа с плавающей точкой" << endl << endl;
+				}
+				else if (result < min || result > max) {
+					cout << "Число вне допустимого диапазона" << min << " - " << max << ")." << endl << endl;
+				}
+				else {
+					return result;
+				}
+			}
+			catch (const invalid_argument&) {
+				cout << "Ошибка ввода числа с плавающей точкой" << endl << endl;
+			}
+			catch (const out_of_range&) {
+				cout << "Число вне диапазона значений" << endl << endl;
+			}
+		}
+	}
+}
+
+template<typename T>
+bool validateInput(const string& input, T& result, int min, int max) {
+	if constexpr (is_same<T, string>::value) {
+		if (input.empty()) {
+			cout << "Ошибка: строка пуста." << endl;
+			return false;
+		}
+		result = input;
+		return true;
+	}
+	else if constexpr (is_same<T, int>::value) {
+		try {
+			size_t pos;
+			int value = stoi(input, &pos);
+			if (pos < input.size()) {
+				cout << "Ошибка: некорректный ввод числа." << endl;
+				return false;
+			}
+			if (value < min || value > max) {
+				cout << "Ошибка: значение вне диапазона" << endl;
+				return false;
+			}
+			result = value;
+			return true;
+		}
+		catch (const invalid_argument&) {
+			cout << "Ошибка: некорректный ввод числа." << endl;
+			return false;
+		}
+		catch (const out_of_range&) {
+			cout << "Ошибка: значение вне диапазона int." << endl;
+			return false;
+		}
+	}
+	else if constexpr (is_same<T, double>::value) {
+		try {
+			size_t pos;
+			double value = stod(input, &pos);
+			if (pos < input.size()) {
+				cout << "Ошибка: некорректный ввод числа с плавающей точкой." << endl;
+				return false;
+			}
+			if (value < min || value > max) {
+				cout << "Ошибка: значение вне диапазона" << endl;
+				return false;
+			}
+			result = value;
+			return true;
+		}
+		catch (const invalid_argument&) {
+			cout << "Ошибка: некорректный ввод числа с плавающей точкой." << endl;
+			return false;
+		}
+		catch (const out_of_range&) {
+			cout << "Ошибка: значение вне диапазона double." << endl;
+			return false;
+		}
+	}
+	return false;
+}
---
+++ lr2_osadc/main.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <chrono>
+#include <type_traits>
+#include <limits>
+#include <cctype>
+
+#include "pipe.h"
+#include "cs.h"
+#include "function.h"
+
+using namespace std;
+
+
+
+void menu()
+{
+	cout << "        Меню: " << endl;
+	cout << " 1. Добавить трубу" << endl;
+	cout << " 2. Добавить КС" << endl;
+	cout << " 3. Вывести объекты" << endl;
+	cout << " 4. Сохранение" << endl;
+	cout << " 5. Загрузить" << endl;
+    cout << " 6. Редактирование по фильтрам" << endl;
+	cout << " 0. Выход" << endl << endl;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	menu();
+
+	int choice;
+	while (true)
+	{
+		choice = getInput<int>("Команда ", 0, 10);
+		system("cls");
+		switch (choice)
+		{
+		case 1:
+			pipeadd();
+			break;
+		case 2:
+			addCS();
+			break;
+		case 3:
+			Print();
+			break;
+		case 4:
+			save();
+			break;
+		case 5:
+			load();
+			break;
+		case 6:
+			filter();
+            break;
+		case 0:
+			cout << "Выход " << endl;
+			return 0;
+		}
+		cout << "Нажмите Enter";
+		cin.ignore(10000, '\n');
+		cerr << endl;
+		system("cls");
+		menu();
+	}
+	return 0;
+}
+
+
